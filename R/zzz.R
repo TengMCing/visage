ebb4e164d79cd2bd22ac18a0f41eecbab5d4e89a
TengMCing <llreczx@gmail.com>
2021-12-27 16:13:22 +1100

    export BASE, class, type, dict

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,53 @@
+#' BASE class environment
+#'
+#' @name BASE
+#'
+#' @description This class provides essential methods.
+#' @format
+#' @export
 BASE <- class_BASE()
+
+#' Class name
+#'
+#' @name BASE$..type..
+#'
+#' @description A string.
+#'
+#' @examples
+#'
+#' BASE$..type..
+BASE$..type..
+
+#' Class name and parent class names
+#'
+#' @name BASE$..type..
+#'
+#' @description A string vector.
+#'
+#' @examples
+#'
+#' BASE$..class..
+BASE$..class..
+
+#' Class name and parent class names
+#'
+#' @name BASE$..class..
+#'
+#' @description A string vector.
+#'
+#' @examples
+#'
+#' BASE$..class..
+BASE$..class..
+
+
+#' All names in the class environment
+#'
+#' @name BASE$..dict..
+#'
+#' @description This function returns all names in the class environment.
+#'
+#' @examples
+#'
+#' BASE$..dict..()
+BASE$..dict..
