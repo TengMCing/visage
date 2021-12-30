a786c9e83bea2af94bd0956d64aca2ff5e6ac288
TengMCing <llreczx@gmail.com>
2021-12-30 17:50:56 +1100

    doc CLOSED_FORM sym, sym_name, sym_type

--- R/zzz_CLOSED_FORM.R
+++ R/zzz_CLOSED_FORM.R
@@ -11,7 +11,7 @@
 #' @seealso Parent class: [BASE]
 #' \cr
 #' \cr
-#' New attributes: CLOSED_FORM$sym, CLOSED_FORM$sym_name,
+#' New attributes: [CLOSED_FORM$sym], CLOSED_FORM$sym_name,
 #' CLOSED_FORM$type, CLOSED_FORM$expr
 #' \cr
 #' \cr
@@ -21,4 +21,65 @@
 #' @export
 CLOSED_FORM <- class_CLOSED_FORM()
 
+#' List of symbols in the abstract syntax tree of the expression
+#'
+#' @name CLOSED_FORM$sym
+#'
+#' @description A list, will be initialized after an instance is built.
+#'
+#' @examples
+#'
+#' # Constant
+#' a <- 1
+#'
+#' # Random uniform variable
+#' b <- RAND_UNIFORM$instantiation()
+#'
+#' # Define a closed form expression
+#' cf <- CLOSED_FORM$instantiation(~3 * (exp(a) + b))
+#'
+#' cf
+#'
+#' # Get the list of symbols
+#' cf$sym
+#'
+#' # Get the list of symbol names
+#' cf$sym_name
+#'
+#' # Get the list of symbol types
+#' cf$sym_type
+#'
+#' d <- RAND_NORMAL$instantiation()
+#'
+#' # Define a closed form expression with another closed form expression
+#' cf2 <- CLOSED_FORM$instantiation(~cf + 3 * d)
+#'
+#' cf2
+#'
+#' # Get the list of symbols, constants are not counted as symbols
+#' cf2$sym
+#'
+#' # Get the list of symbol names
+#' cf2$sym_name
+#'
+#' # Get the list of symbol types
+#' cf$sym_type
+CLOSED_FORM$sym
 
+#' List of symbol names in the abstract syntax tree of the expression
+#'
+#' @name CLOSED_FORM$sym_name
+#'
+#' @description A list, will be initialized after an instance is built.
+#'
+#' @seealso Examples and usages: [CLOSED_FORM$sym]
+CLOSED_FORM$sym_name
+
+#' List of symbol types in the abstract syntax tree of the expression
+#'
+#' @name CLOSED_FORM$sym_type
+#'
+#' @description A list, will be initialized after an instance is built.
+#'
+#' @seealso Examples and usages: [CLOSED_FORM$sym]
+CLOSED_FORM$sym_type
