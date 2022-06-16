e400cdee955ec8df18a379b416dbe986b7d18929
TengMCing <llreczx@gmail.com>
2022-06-16 20:56:22 +1000

    modify tests

--- tests/testthat/test-eval_p_value.R
+++ tests/testthat/test-eval_p_value.R
@@ -1,8 +1,8 @@
 test_that("eval_p_value() works", {
-  expect_equal(eval_p_value(0.054), FALSE)
+  expect_equal(visage::eval_p_value(0.054), FALSE)
 })
 
 test_that("eval_p_value() can use tol correctly", {
-  expect_equal(eval_p_value(0.054, tol = 0.001), FALSE)
-  expect_equal(eval_p_value(0.054, tol = 0.01), TRUE)
+  expect_equal(visage::eval_p_value(0.054, tol = 0.001), FALSE)
+  expect_equal(visage::eval_p_value(0.054, tol = 0.01), TRUE)
 })
