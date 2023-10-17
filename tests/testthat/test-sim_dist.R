0c61b6907e1d6f5fe810c77ea803a34196a0b745
TengMCing <llreczx@gmail.com>
2023-10-17 11:39:32 +1100

    add test for sim_dist

--- tests/testthat/test-sim_dist.R
+++ tests/testthat/test-sim_dist.R
@@ -6,3 +6,7 @@ test_that("sim_dist() works and adds up to 1", {
 test_that("sim_dist() passes tol = 0.01 for n_eval = 1, n_sel = 1 (if not, increase n_sim)", {
   expect_lt(abs(visage::sim_dist(c(1))[1] - 0.95), 0.01)
 })
+
+test_that("sim_dist() works with Dirichlet distribution and adds up to 1", {
+  expect_equal(sum(visage::sim_dist(c(2, 2, 3), n_sim = 1000, dist = "dirichlet")), 1)
+})
